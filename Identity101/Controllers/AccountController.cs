702f8f6e0ad2ad48a0679b4a4076bc2d276ff382
wissen.mert@outlook.com <wissen.mert@outlook.com>
2022-04-12 11:57:01 +0300

    Roles and login function implemented

--- Identity101/Controllers/AccountController.cs
+++ Identity101/Controllers/AccountController.cs
@@ -1,22 +1,62 @@
-﻿using Identity101.Models.Identity;
+﻿using Identity101.Models.Email;
+using Identity101.Models.Identity;
+using Identity101.Models.Role;
+using Identity101.Services.Email;
 using Identity101.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+using System.Text;
+using System.Text.Encodings.Web;
 
 namespace Identity101.Controllers;
 
 public class AccountController : Controller
 {
     private readonly UserManager<ApplicationUser> _userManager;
+    private readonly IEmailService _emailService;
+    private readonly RoleManager<ApplicationRole> _roleManager;
 
-    public AccountController(UserManager<ApplicationUser> userManager)
+    public AccountController(UserManager<ApplicationUser> userManager, IEmailService emailService, RoleManager<ApplicationRole> roleManager)
     {
         _userManager = userManager;
+        _emailService = emailService;
+        _roleManager = roleManager;
+        CheckRoles();
     }
+
+    private void CheckRoles()
+    {
+        foreach (string item in Roles.RoleList)
+        {
+            if (_roleManager.RoleExistsAsync(item).Result)
+            {
+                continue;
+            }
+            var result = _roleManager.CreateAsync(new ApplicationRole()
+            {
+                Name = item,
+            }).Result;
+        }
+    }
+
 
     [HttpGet("~/kayit-ol")]
     public IActionResult Register()
     {
+        _emailService.SendMailAsync(new MailModel()
+        {
+            To = new List<EmailModel>()
+                {
+                    new EmailModel()
+                    {
+                        Name ="Wissen",
+                        Adress = "akcaymert603@gmail.com"
+                    }
+                },
+            Subject = "Logged in....",
+            Body = "🚀 Successful login 🚀 "
+        });
         return View();
     }
 
@@ -37,17 +77,63 @@ public class AccountController : Controller
             Surname = model.Surname
         };
 
+
+
         var result = await _userManager.CreateAsync(user, model.Password);
         if (result.Succeeded)
         {
-            //TODO: Email gönderme - Aktivasyon
+            //Rol Atama
+            var count = _userManager.Users.Count();
+            result = await _userManager.AddToRoleAsync(user, count == 1 ? Roles.Admin : Roles.Passive);
+
+            //Email gönderme - Aktivasyon
+            var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+            var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Scheme);
+
+            var emailMessage = new MailModel()
+            {
+                To = new List<EmailModel> { new EmailModel()
+                {
+                    Adress = user.Email,
+                    Name = user.Name
+                }},
+                Body = $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here </a>.",
+                Subject = "Confirm your email"
+            };
+
+
             //TODO: Login olma
             return RedirectToAction("Login");
         }
-        var messages = string.Join("\n", result.Errors.Select(e => e.Description));
+        var messages = string.Join("<br>", result.Errors.Select(e => e.Description));
         ModelState.AddModelError(string.Empty, messages);
         return View(model);
 
+    }
+
+    public async Task<IActionResult> ConfirmEmail(string userId, string code)
+    {
+        if(userId == null || code == null)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+        var user = await _userManager.FindByIdAsync(userId);
+        if (user == null) return NotFound($"Unable to load user with ID ${userId}");
+
+        code = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(code));
+        var result = await _userManager.ConfirmEmailAsync(user, code);
+        ViewBag.StatusMessage = result.Succeeded
+            ? "Thank you for confirming your email"
+            : "Error confirming your email.";
+
+        if(result.Succeeded && _userManager.IsInRoleAsync(user, Roles.Passive).Result)
+        {
+            await _userManager.RemoveFromRoleAsync(user, Roles.Passive);
+            await _userManager.AddToRoleAsync(user, Roles.User);
+        }
+
+        return View();
     }
 
     public IActionResult Login()
---
+++ Identity101/Services/Email/OutlookEmailService.cs
@@ -0,0 +1,73 @@
+﻿using Identity101.Models.Configuration;
+using Identity101.Models.Email;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace Identity101.Services.Email;
+
+
+public class OutlookEmailService : IEmailService
+{
+    private readonly IConfiguration _config;
+    public EmailSettings EmailSettings { get; }
+
+    public OutlookEmailService(IConfiguration config)
+    {
+        _config = config;
+        this.EmailSettings = _config.GetSection("OutlookSettings").Get<EmailSettings>();
+        
+    }
+
+    
+
+
+    public Task SendMailAsync(MailModel model)
+    {
+        var mail = new MailMessage { From = new MailAddress(this.EmailSettings.SenderMail) };
+
+        foreach (var c in model.To)
+        {
+            mail.To.Add(new MailAddress(c.Adress, c.Name));
+        }
+
+        foreach (var cc in model.Cc)
+        {
+            mail.CC.Add(new MailAddress(cc.Adress, cc.Name));
+        }
+
+        foreach (var bcc in model.Bcc)
+        {
+            mail.Bcc.Add(new MailAddress(bcc.Adress, bcc.Name));
+        }
+
+        if (model.Attachs is { Count: > 0 })
+        {
+            foreach (var modelAttach in model.Attachs)
+            {
+                var fileStream = modelAttach as FileStream;
+                var info = new FileInfo(fileStream.Name);
+                mail.Attachments.Add(new Attachment(fileStream, info.Name));
+            }
+        }
+
+
+        mail.Subject = model.Subject;
+        mail.Body = model.Body;
+        mail.IsBodyHtml = true;
+        mail.BodyEncoding = Encoding.UTF8;
+        mail.SubjectEncoding = Encoding.UTF8;
+        mail.HeadersEncoding = Encoding.UTF8;
+
+        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+        var smtpClient = new SmtpClient(this.EmailSettings.Smtp, this.EmailSettings.SmtpPort)
+        {
+            Credentials = new NetworkCredential(userName: this.EmailSettings.SenderMail, this.EmailSettings.Password),
+            EnableSsl = true
+        };
+
+        return smtpClient.SendMailAsync(mail);
+
+    }
+}
